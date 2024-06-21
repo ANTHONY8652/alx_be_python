ea5568ac0c6e110340c29a407b9aac7149289864
ANTHONY8652 <githinjianthony720@gmail.com>
2024-06-22 00:20:04 +0300

    Simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#Define the variables
+principal = 1000 #principle amount in dollars
+rate = 0.05 #represents 5% annual interest rate
+time = 3 #Represents time in years
+
+#calculate the simple interest
+interest = principal * rate * time
+
+#print the simple calculated interest
+print(f"The simple interest is: {interest}")
+
