017b099e5701b952d8e28a233d4ece1ed4acf90c
ANTHONY8652 <githinjianthony720@gmail.com>
2024-06-22 00:06:42 +0300

    Basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#assign specific value to the variable
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+Addition_result = number1 + number2
+Subtraction_result = number1 - number2
+Multiplication_result = number1 * number2
+
+#Print sum result subtraction result and division result in human readable format 
+print(f"Addition of {number1} and {number2} is {Addition_result}")
+print(f"Subtraction of {number1} and {number2} is {Subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication_result}")
