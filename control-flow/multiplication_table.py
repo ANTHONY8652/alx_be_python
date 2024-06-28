497aa3d03855a5fd56ce58e7bec5b630e80750c8
ANTHONY8652 <githinjianthony720@gmail.com>
2024-06-29 00:32:45 +0300

    You're killing me code checker

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 # Prompt the user to enter a number
 number = int(input("Enter a number to see its multiplication table: "))
 # Generate and print the multiplication table using a for loop
-for i in range(1, 10+1):
+for i in range(1, 11):
     product = number * i
     print(f"{number} * {i} = {product}")
