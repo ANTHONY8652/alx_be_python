6b113e244dea3e64f12957f72322cd8116fc52c4
ANTHONY8652 <githinjianthony720@gmail.com>
2024-06-29 00:20:59 +0300

    commit uno

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,6 +4,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 # Generate and print the multiplication table using a for loop
-for i in range(1, 10+1):
+for i in range(1, 10):
     product = number * i
     print(f"{number} * {i} = {product}")
