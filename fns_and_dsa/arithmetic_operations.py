5cf43debf6a6e2a8a14b46a145b9a582489ee9dc
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-08 00:31:18 +0300

    Alejandro

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
+        if num2 == 0:
+            return " Error: Division by zero is not allowed"
         else:
-            return "Error: Division by zero"
+            return num1 / num2
+    else:
+        return " Error: unsupported operation"             
--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,31 +1,28 @@
+# Define Global Conversion Factors:
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return celsius
-
-def convert_to_fahrenheit(celsius):
-    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+# Implement Conversion Functions:
+def convert_to_celsius(celsius):
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     return fahrenheit
+def convert_to_fahrenheit(fahrenheit):
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius 
 
-def get_user_input():
-    while True:
-        try:
-            temperature = float(input("Enter the temperature to convert: "))
-            unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
-            if unit not in ['C', 'F']:
-                raise ValueError("Invalid temperature. Please enter a valid unit.")
-            return temperature, unit
-        except ValueError as e:
-            print(e)
-
-if __name__ == "__main__":
-    temperature, unit = get_user_input()
-
+# User Interaction:
+def main():
+    temperature = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
     if unit == 'F':
-        converted_celsius = convert_to_celsius(temperature)
-        print(f"{temperature}°F is {converted_celsius}°C")
+        converted_temp = convert_to_celsius(temperature)
+        print(f"{temperature} is {converted_temp: .2f}Celsius")
     elif unit == 'C':
-        converted_fahrenheit = convert_to_fahrenheit(temperature)
-        print(f"{temperature}°C is {converted_fahrenheit}°F")
+        converted_temp = convert_to_fahrenheit(temperature)
+        print(f"{temperature}. is {converted_temp: .2f}Fahrenheit")
+    else:
+        print("Invalid temperature. Please enter a numeric value.")
+
+#Execute script
+if __name__ ==  "__main__":
+    main()
