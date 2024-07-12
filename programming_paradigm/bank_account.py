adbd59ccb6b0d7c438ce0319dc34783ebce25f40
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-12 03:15:46 +0300

    take me to heaven now

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,3 @@
-""""Objective: Understand the fundamentals of OOP in Python by implementing a BankAccount class that encapsulates banking operations. Use command line arguments to interact with instances of this class.
-
-Task Description:
-You will create two Python scripts: bank_account.py, which contains the BankAccount class, and main-0.py, which interfaces with the class through command line arguments to perform banking operations.
-
-bank_account.py:
-Class Definition:
-
-Define a class named BankAccount.
-Use the __init__ method to initialize an account_balance attribute. Optionally, accept an initial balance parameter, defaulting to zero.
-Encapsulation and Behaviors:
-
-Implement deposit(amount), withdraw(amount), and display_balance() methods.
-deposit should add the specified amount to account_balance.
-withdraw should deduct the amount from account_balance if funds are sufficient, returning True; otherwise, return False and do not alter the balance.
-display_balance should print the current balance in a user-friendly format"""""
 class BankAccount:
     def __init__ (self, initial_balance=0):
         self._account_balance = initial_balance
@@ -35,4 +19,4 @@ class BankAccount:
         return self._account_balance
     
     def display_balance(self):
-        print(f"Current balance is ${self.get_balance()}")
+        print["Current balance is ${self.get_balance()}"]
