79418b266553e31efca96a55c42697f8b624a709
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-12 05:22:45 +0300

    robust calculator take 88

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+    
+    except ZeroDivisionError:
+        return "Error: cannot divide by zero"
+    
+    except ValueError:
+        return "Error: Please enter numeric values only"
+    
+    finally:
+        print(f"The result of the division is {result:.1f}")
