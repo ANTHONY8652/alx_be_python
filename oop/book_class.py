7e8e99233a84c8379a002bc5a6728651993d7c30
ANTHONY8652 <githinjianthony720@gmail.com>
2024-07-20 20:52:19 +0300

    smol smol error

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,15 +5,10 @@ class Book:
         self.year = year
 
     def __del__(self):
-        print("Deleting " + self.title)
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        return self.title + " by " + self.author + " published in " + str(self.year )
+        return f"{self.title} by {self.author} published in {self.year}"
     
     def __repr__(self):
-        return "Book('" + self.title + "', '" + self.author + "', '" + str(self.year) + ")"
-    
-book = Book("1984", "George Orwell", 1946)
-print(str(book))
-print(repr(book))
-del book
+        return f"Book('{self.title}', '{self.author}', {self.year})"
